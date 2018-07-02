555f53d405a5b230295a0e3c14d04e24a6c627c5
wzheng <hb_wangzheng@163.com>
2018-07-02 18:02:31 +0800

    add news

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,30 +9,107 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
 
-    bannerList: [{
-      id: 'football',
-      url: '/pages/my/my',
-      img: '/images/banner_1.png'
-    }, {
-      id: 'football',
-      url: '/pages/my/my',
-      img: '/images/banner_2.png'
-    }],
+    bannerList: [
+      {
+        id: 'football',
+        url: '/pages/my/my',
+        img: '/images/banner_1.png'
+      }, {
+        id: 'football',
+        url: '/pages/my/my',
+        img: '/images/banner_2.png'
+      }
+    ],
 
-    menuList: [{
-      id: 1,
-      url: '/pages/my/my',
-      iconClass: 'car',
-      name: '人找车'
-    }]
+    menuList: [
+      {
+        id: 1,
+        url: '/pages/my/my',
+        iconClass: 'for-car',
+        name: '人找车'
+      }, {
+        id: 2,
+        url: '/pages/my/my',
+        iconClass: 'sale',
+        name: '出售'
+      }, {
+        id: 3,
+        url: '/pages/my/my',
+        iconClass: 'employ',
+        name: '招聘'
+      }, {
+        id: 4,
+        url: '/pages/my/my',
+        iconClass: 'transfer',
+        name: '转让'
+      }, {
+        id: 5,
+        url: '/pages/my/my',
+        iconClass: 'help',
+        name: '求助'
+      }, {
+        id: 6,
+        url: '/pages/my/my',
+        iconClass: 'for-person',
+        name: '车找人'
+      }, {
+        id: 7,
+        url: '/pages/my/my',
+        iconClass: 'land',
+        name: '出租'
+      }, {
+        id: 8,
+        url: '/pages/my/my',
+        iconClass: 'for-job',
+        name: '求职'
+      }, {
+        id: 9,
+        url: '/pages/my/my',
+        iconClass: 'for-goods',
+        name: '求购'
+      }, {
+        id: 0,
+        url: '/pages/my/my',
+        iconClass: 'asking',
+        name: '打听'
+      },
+    ],
+
+    recomNews: [
+      {
+        id: 1,
+        title: '勒布朗-詹姆斯的经纪团队正式宣布，詹姆斯与湖人达成4年1.54亿美元口头协议，其中第四年为球员选项，双方将等到当地时间7月6日合约冻结期结束后正式签约。',
+        url: '/pages/my/my',
+      },
+      {
+        id: 2,
+        title: '2018年休赛期最大悬念至此揭晓，詹姆斯结束与老东家骑士的合作，NBA生涯首次转战西部',
+        url: '/pages/my/my',
+      },
+      // {
+      //   id: 3,
+      //   title: '生涯首次转战西部',
+      //   url: '/pages/my/my',
+      // }
+    ],
   },
   //事件处理函数
   bindViewTap: function () {
-    wx.navigateTo({
+    wx.switchTab({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
+    let recomNews = this.data.recomNews
+    recomNews.forEach((item, index) => {
+      console.log(item, index, 'asdfg'.slice(0, 2));
+
+      item['title'] = item['title'].slice(0, 20) + '...'
+    });
+    // this.setData({ recomNews })
+    console.log()
+
+    //
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
